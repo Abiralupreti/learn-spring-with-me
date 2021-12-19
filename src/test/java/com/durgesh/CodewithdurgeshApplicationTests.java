4658713d2a3023eef0af74ed62e3cf2c16ddd2f7
sunderkrishnaupreti <sunderkrishna_upreti@yahoo.com>
2021-12-18 18:52:17 -0600

    project created

---
+++ src/test/java/com/durgesh/CodewithdurgeshApplicationTests.java
@@ -0,0 +1,13 @@
+package com.durgesh;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodewithdurgeshApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
